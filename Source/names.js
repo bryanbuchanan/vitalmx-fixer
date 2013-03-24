5fddfdb1134819d90cacea493a18f7f30eb06414
Bryan Buchanan <bryanbuchanan@new-host-2.home>
2013-03-24 14:41:35 -0700

    Initial commit

---
+++ Source/names.js
@@ -0,0 +1,42 @@
+var names = {
+	"RV(2|1)?": "Ryan Villopoto",
+	"BB(4|1)?": "Blake Baggett",
+	"RD(5)?": "Ryan Dungey",
+	"JS(7)?": "James Stewart",
+	"IT(9)?": "Ivan Tedesco",
+	"JB(10)?": "Justin Brayton",
+	"KC(11)?": "Kyle Chisholm",
+	"JW(12)?": "Jake Weimer",
+	"BW(13)?": "Blake Wharton",
+	"KW(14)?": "Kevin Windham",
+	"DW(15)?": "Dean Wilson",
+	"JD(16)?": "John Dowd",
+	"ET(17|1)?": "Eli Tomac",
+	"DM(18)?": "Davi Millsaps",
+	"WH(19)?": "Will Haun",
+	"BT(20)?": "Broc Tickle",
+	"CR(22)?": "Chad Reed",
+	"BM(24)?": "Bret Metcalfe",	
+	"MM(25)?": "Marvin Musquin",
+	"MB(26)?": "Michael Byrne",
+	"NW(27)?": "Nick Wey",
+	"TR(28)?": "Tyla Rattray",
+	"AS(29)?": "Andrew Short",
+	"JG(33)?": "Josh Grant",
+	"TC(41)?": "Trey Canard",
+	"JB(51)?": "Justin Barcia",
+	"JH(75)?": "Josh Hill",
+	"KR(94)?": "Ken Roczen",
+	"MA(800)?": "Mike Alessi"
+};
+
+var content = document.body.innerHTML;
+for (var name in names) var content = content.replace(new RegExp(name, 'g'), names[name]);
+document.body.innerHTML = content;
+
+/* Target only post content (not done)
+var menus = document.getElementsByClassName('body');
+for (var i = menus.length - 1; i >= 0; i--) {
+  menus[i].className = "style1";
+}
+*/
---
+++ readme.md
@@ -0,0 +1,6 @@
+# VitalMX.com Rider Name Acronym Expander
+
+This is a Google Chrome extension that changes those obscure rider references like RV2 into the riders' proper names.
+
+- Only affects vitalmx.com forums, nothing else.
+- Might screw something up, who knows.
