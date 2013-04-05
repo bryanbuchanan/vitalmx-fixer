6ebe5e1c233b01de3a709631ed9f05882df1b5df
Bryan Buchanan <bryanbuchanan@new-host-3.home>
2013-04-05 11:18:24 -0700

    Improved matching pattern
    Less wreckless, should replace fewer things it shouldn't.

--- Source/names.js
+++ Source/names.js
@@ -3,7 +3,7 @@ var names = {
 	"BB(4|1)?": "Blake Baggett",
 	"RD(5)?": "Ryan Dungey",
 	"JS(7)?": "James Stewart",
-	"IT(9)": "Ivan Tedesco", // Number required to avoid conflicts with common words
+	"IT(9)": "Ivan Tedesco", // IT is too common of a word, so number is required
 	"JB(10)?": "Justin Brayton",
 	"KC(11)?": "Kyle Chisholm",
 	"JW(12)?": "Jake Weimer",
@@ -15,21 +15,34 @@ var names = {
 	"DM(18)?": "Davi Millsaps",
 	"WH(19)?": "Will Haun",
 	"BT(20)?": "Broc Tickle",
-	"CR(22)": "Chad Reed", // Number required to avoid conflicts with bike name
+	"CR(22)": "Chad Reed", // CR is too common of a word, number required
 	"BM(24)?": "Bret Metcalfe",	
-	"MM(25)": "Marvin Musquin", // Number required to avoid conflicts with common words
+	"MM(25)?": "Marvin Musquin",
 	"MB(26)?": "Michael Byrne",
 	"NW(27)?": "Nick Wey",
 	"TR(28)?": "Tyla Rattray",
 	"AS(29)?": "Andrew Short",
-	"JG(33)": "Josh Grant", // JGR
-	"TC(41)": "Trey Canard", // Number required to avoid conflicts with bike name
+	"JG(33)?": "Josh Grant",
+	"TC(41)?": "Trey Canard",
 	"JB(51)?": "Justin Barcia",
 	"JH(75)?": "Josh Hill",
 	"KR(94)?": "Ken Roczen",
-	"MA(800)": "Mike Alessi" // Number required to avoid conflicts with common words
+	"MA(800)?": "Mike Alessi"
 };
 
+for (var name in names) {
+
+	// Add specifics to patterns
+	var abbr = "(\\W)" + name + "(\\W)";
+	var realName = "$1" + names[name] + "$3";
+	
+	// Replace old pattern with new pattern
+	names[abbr] = realName;
+	delete names[name];
+
+}
+
+// Replace content
 var content = document.body.innerHTML;
 for (var name in names) var content = content.replace(new RegExp(name, 'g'), names[name]);
 document.body.innerHTML = content;
